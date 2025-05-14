caf5ba22ac12b6a6c9d761fed95b728bda48eebf
Jhocep Verastegui <snkje123@gmail.com>
2025-05-14 17:07:56 -0500

    14/05/25

--- Sem08/Program.cs
+++ Sem08/Program.cs
@@ -10,14 +10,31 @@ namespace Sem08
     {
         static void Main(string[] args)
         {
+           Metodo2();
+        }
+
+        static void Metodo1()
+        { 
             int x, y = 1;
             Console.WriteLine("Ingrese un número: ");
             x = int.Parse(Console.ReadLine());
             for (int i = 1; i <= x; i++)
             {
-                y *= i;
+                y = y * i;
             }
             Console.WriteLine("El factorial de " + x + " es: " + y);
+        }
+        static int Metodo2()
+        {
+            int x, y = 1;
+            Console.WriteLine("Ingrese un número: ");
+            x = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= x; i++)
+            {
+                y = y * i;
+            }
+            Console.WriteLine("El factorial es: " + y);
+            return y;
         }
     }
 }
