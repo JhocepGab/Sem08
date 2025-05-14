b6bb969b77b04b1fb7723351ccf6318d9b51720d
Jhocep Verastegui <snkje123@gmail.com>
2025-05-14 16:40:36 -0500

    Agregar archivos de proyecto.

---
+++ Sem08/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y = 1;
+            Console.WriteLine("Ingrese un número: ");
+            x = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= x; i++)
+            {
+                y *= i;
+            }
+            Console.WriteLine("El factorial de " + x + " es: " + y);
+        }
+    }
+}
